d83d6bb43fef587522ede972e689febe99afc467
llastivka <lenalastivka@gmail.com>
2018-11-27 23:13:30 +0100

    PROACS-1 started implementing angles choosing

--- app/src/main/java/com/sum/scanner/mysuccessfulthesis/CaptureFragment.java
+++ app/src/main/java/com/sum/scanner/mysuccessfulthesis/CaptureFragment.java
@@ -11,13 +11,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.logging.Logger;
 
 public class CaptureFragment extends Fragment {
@@ -26,6 +26,11 @@ public class CaptureFragment extends Fragment {
 
     private Context mContext;
     private Activity mFragmentActivity;
+
+    private ImageView angle1, angle2, angle3, angle4;
+    private int xDelta[] = new int[4];
+    private int yDelta[] = new int[4];
+    private ViewGroup rootLayout;
 
     @Override
     public void onAttach(Context context) {
@@ -39,33 +44,32 @@ public class CaptureFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_capture, container, false);
 
-//        Bundle bundle = this.getArguments();
-//        if (bundle != null) {
-//            byte[] imageBytes = bundle.getByteArray("capture");
         byte[] imageBytes = ((MainActivity) getActivity()).getImageBytes();
-            if (imageBytes != null) {
-                logger.info("Decoding image bytes array into bitmap");
-                ImageView image = view.findViewById(R.id.camera_capture);
+        if (imageBytes != null) {
+            logger.info("Decoding image bytes array into bitmap");
+            ImageView image = view.findViewById(R.id.camera_capture);
+            //I added 2 lines to manifest to avoid this error but still consider some scaling
 //                BitmapFactory.Options options = new BitmapFactory.Options();
 //                options.inSampleSize = 2;
-                Bitmap decodedBitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
-                Bitmap rotatedBitmap = rotate(decodedBitmap);
-//                Drawable d = Drawable.createFromStream(new ByteArrayInputStream(imageBytes), null);
-//                image.setImageDrawable(d);
-                image.setImageBitmap(rotatedBitmap);
+            Bitmap decodedBitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+            Bitmap rotatedBitmap = rotate(decodedBitmap);
+            image.setImageBitmap(rotatedBitmap);
+        } else {
+            logger.info("Image bytes array is null");
+        }
 
-                try (FileOutputStream out = new FileOutputStream("pippo.pmg")) {
-                    rotatedBitmap.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
-                    // PNG is a lossless format, the compression factor (100) is ignored
-                } catch (IOException e) {
-                }
+        rootLayout = view.findViewById(R.id.angles_layout);
+        angle1 = rootLayout.findViewById(R.id.angle1);
+        angle2 = rootLayout.findViewById(R.id.angle2);
+        angle3 = rootLayout.findViewById(R.id.angle3);
+        angle4 = rootLayout.findViewById(R.id.angle4);
 
-            } else {
-                logger.info("Image bytes array is null");
-            }
-//        } else {
-//            logger.info("Bundle is null");
-//        }
+//        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
+//        img.setLayoutParams(layoutParams);
+        angle1.setOnTouchListener(new ChoiceTouchListener(0));
+        angle2.setOnTouchListener(new ChoiceTouchListener(1));
+        angle3.setOnTouchListener(new ChoiceTouchListener(2));
+        angle4.setOnTouchListener(new ChoiceTouchListener(3));
 
         Button tryAgainButton = view.findViewById(R.id.try_again_capture_button);
         tryAgainButton.setOnClickListener(new View.OnClickListener() {
@@ -97,5 +101,43 @@ public class CaptureFragment extends Fragment {
         Matrix matrix = new Matrix();
         matrix.setRotate(90);
         return Bitmap.createBitmap(decodedBitmap, 0, 0, width, height, matrix, true);
+    }
+
+    private final class ChoiceTouchListener implements View.OnTouchListener {
+
+        private int angleId;
+
+        public ChoiceTouchListener(int angleId) {
+            this.angleId = angleId;
+        }
+
+        public boolean onTouch(View view, MotionEvent event) {
+            final int X = (int) event.getRawX();
+            final int Y = (int) event.getRawY();
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    xDelta[angleId] = X - lParams.leftMargin;
+                    yDelta[angleId] = Y - lParams.topMargin;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    break;
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+                            .getLayoutParams();
+                    layoutParams.leftMargin = X - xDelta[angleId];
+                    layoutParams.topMargin = Y - yDelta[angleId];
+                    layoutParams.rightMargin = -250;
+                    layoutParams.bottomMargin = -250;
+                    view.setLayoutParams(layoutParams);
+                    break;
+            }
+            rootLayout.invalidate();
+            return true;
+        }
     }
 }
